8e66cb0d74a21d6679503ccda9f74f19d3f665fe
Abhi01goenka <abhishekgoenka4@gmail.com>
2024-12-11 13:13:43 +0530

    Courses Offered page design complete

---
+++ src/pages/coursesOffered.tsx
@@ -0,0 +1,303 @@
+export default function coursesOffered() {
+    return (
+        <>
+            <header className="bg-neutral-900 text-white text-center py-10">
+                <h1 className="text-4xl font-bold">Courses Offered</h1>
+            </header>
+
+            {/* Yoga Prana Vidya Level - 1 */}
+            <div className="min-h-screen flex flex-col items-center justify-center space-y-8 px-4 pt-[2rem] pb-[2rem]">
+                <h1 className="text-4xl font-bold text-center">Yoga Prana Vidya Level - 1</h1>
+                <div className="flex flex-col md:flex-row items-center md:space-x-6 space-y-6 md:space-y-0">
+                    {/* Image Section */}
+                    <div className="md:w-1/2 order-1 md:order-2">
+                        <img
+                            src="/YPV-Level-1.jpeg"
+                            alt="YPV Level-1"
+                            className="rounded-lg shadow-lg w-full object-contain max-h-[400px]"
+                        />
+                    </div>
+
+                    {/* Text Section */}
+                    <div className="md:w-1/2 text-center md:text-left space-y-4 order-2 md:order-1 text-lg">
+                        <p className="text-neutral-400">
+                            <b>Pre-requisite :</b> Anyone above 14 years
+                        </p>
+                        <p className="text-neutral-400">
+                            <b>Duration :</b> 2 days
+                        </p>
+                        <p className="text-neutral-400">
+                            <b>Workshop Features:</b><br />
+                            <li>This course is an introduction into meditation, pranayama and the most advanced Energy Healing on earth.</li>
+                            <li>Understand the pranayama kosha or the energy body, the chakrams and the meridians.</li>
+                            <li>Learn to heal the energy body using various techniques in order to treat several ailments.</li>
+                            <li>Learn Self-healing and Distance healing.</li>
+                            <li>Practice the rejuvenating Rhythmic Deep Abdominal Breathing technique.</li>
+                            <li>Experience advance guided Planetary Peace Meditation (PPM) for Inner peace and calmness, reducing stress, increase concentration power, happiness, health relationships, etc.</li>
+                            <li>Learn to heal yourself and your loved ones ranging from common cold to asthma, arthritis, hypertension, gastrointestinal ailments, etc.</li>
+                            <li><b>You will be equipped with guided audios, healing tracks, handouts (book), nurturing sessions to continue your healing practices after the completion of the course.</b></li>
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            {/* Yoga Prana Vidya Level - 2 */}
+            <div className="bg-neutral-900 min-h-screen flex flex-col items-center justify-center space-y-8 px-4 pt-[2rem] pb-[2rem]">
+                <h1 className="text-4xl font-bold text-center">Yoga Prana Vidya Level - 2</h1>
+                <div className="flex flex-col md:flex-row items-center md:space-x-6 space-y-6 md:space-y-0">
+                    {/* Image Section */}
+                    <div className="md:w-1/2 order-1 md:order-2">
+                        <img
+                            src="/YPV-Level-2.jpeg"
+                            alt="YPV Level-2"
+                            className="rounded-lg shadow-lg w-full object-contain max-h-[400px]"
+                        />
+                    </div>
+
+                    {/* Text Section */}
+                    <div className="md:w-1/2 text-center md:text-left space-y-4 order-2 md:order-1 text-lg">
+                        <p className="text-neutral-400">
+                            <b>Pre-requisite :</b> YPV Level 1
+                        </p>
+                        <p className="text-neutral-400">
+                            <b>Duration :</b> 2 days
+                        </p>
+                        <p className="text-neutral-400">
+                            <b>Workshop Features:</b><br />
+                            <li>Chronic and severe cases can be handled more easily with YPV Level 2.</li>
+                            <li>The Healing properties of different colored light is combined with Energy Healing to yield better, more pronounced results in healing.</li>
+                            <li>Learn how to generate Energy of specific frequencies and of varying intensities to increase the rate of healing of the physical body.</li>
+                            <li>Learn specific techniques and protocols to heal and regenerate delicate internal organs, rejuvenate the lymphatic system and boost immunity. </li>
+                            <li>Learn to facilitate the recovery of patients with complex medical cases of the musculoskeletal, cardiovascular, respiratory, digestive and endocrine systems.</li>
+                            <li>Perform Energy first aid and increase the speed of recuperation of the physical body in cases of cuts, bruises and fresh wounds.</li>
+                            <li>Pick up the advanced techniques and the use of colors for quicker and more effective healings.</li>
+                            <li>Learn powerful skills of divine healing, instructive healing and mass healing that enables you to heal a large number of people at once.</li>
+                            <li>Expand on the technique of Planetary Peace Meditation to heal your body of physical ailments while you meditate.</li>
+                            <li><b>The student will be equipped with guided audios, healing tracks and handouts to continue their healing practices after the completion of the course.</b></li>
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            {/* Yoga Prana Vidya Level - 3 */}
+            <div className="min-h-screen flex flex-col items-center justify-center space-y-8 px-4 pt-[2rem] pb-[2rem]">
+                <h1 className="text-4xl font-bold text-center">Yoga Prana Vidya Level - 3</h1>
+                <div className="flex flex-col md:flex-row items-center md:space-x-6 space-y-6 md:space-y-0">
+                    {/* Image Section */}
+                    <div className="md:w-1/2 order-1 md:order-2">
+                        <img
+                            src="/YPV-Level-3.jpeg"
+                            alt="YPV Level-3"
+                            className="rounded-lg shadow-lg w-full object-contain max-h-[400px]"
+                        />
+                    </div>
+
+                    {/* Text Section */}
+                    <div className="md:w-1/2 text-center md:text-left space-y-4 order-2 md:order-1 text-lg">
+                        <p className="text-neutral-400">
+                            <b>Pre-requisite :</b> YPV Level 2
+                        </p>
+                        <p className="text-neutral-400">
+                            <b>Duration :</b> 1 day
+                        </p>
+                        <p className="text-neutral-400">
+                            <b>Workshop Features:</b><br />
+                            <li>Techniques to heal all kinds of psychological problems like <b>stress, anger, phobia, anxiety, addictions, depression, violence, multiple personality disorder, hallucinations</b> etc.</li>
+                            <li>Know to heal <b>relationships</b> too.</li>
+                            <li>Learn sublimation of teenagers </li>
+                            <li>Learn PPM with psychological healing and other techniques of self-healing.</li>
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            {/* Achieving Union With Atma (AUWA) */}
+            <div className="bg-neutral-900 min-h-screen flex flex-col items-center justify-center space-y-8 px-4 pt-[2rem] pb-[2rem]">
+                <h1 className="text-4xl font-bold text-center">Achieving Union With Atma (AUWA)</h1>
+                <div className="flex flex-col md:flex-row items-center md:space-x-6 space-y-6 md:space-y-0">
+                    {/* Image Section */}
+                    <div className="md:w-1/2 order-1 md:order-2">
+                        <img
+                            src="/AUWA.jpeg"
+                            alt="AUWA"
+                            className="rounded-lg shadow-lg w-full object-contain max-h-[400px]"
+                        />
+                    </div>
+
+                    {/* Text Section */}
+                    <div className="md:w-1/2 text-center md:text-left space-y-4 order-2 md:order-1 text-lg">
+                        <p className="text-neutral-400">
+                            <b>Pre-requisite :</b> Anyone above 14 years of age
+                        </p>
+                        <p className="text-neutral-400">
+                            <b>Duration :</b> 1 day
+                        </p>
+                        <p className="text-neutral-400">
+                            <b>Workshop Features:</b><br />
+                            <li>Understand your true spiritual nature and purpose of life.</li>
+                            <li>What is the Inner science of Birth and Death?</li>
+                            <li>How are we connected to our family, friends and relatives even before Death?</li>
+                            <li>How to release past karma and emotional baggage?</li>
+                            <li>What are the seeds of life?</li>
+                            <li>How to live in this world?</li>
+                            <li>Experience clarity in thoughts, Inner peace and calmness in the midst of a busy and chaotic work/home environment.</li>
+                            <li>Awaken the light within you through meditation on Atma and Planetary Peace meditation (PPM). Know their benefits.</li>
+                            <li>Develop your will power.</li>
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            {/* Yoga Prana Vidya Level with Crystals */}
+            <div className="min-h-screen flex flex-col items-center justify-center space-y-8 px-4 pt-[2rem] pb-[2rem]">
+                <h1 className="text-4xl font-bold text-center">Yoga Prana Vidya Level with Crystals</h1>
+                <div className="flex flex-col md:flex-row items-center md:space-x-6 space-y-6 md:space-y-0">
+                    {/* Image Section */}
+                    <div className="md:w-1/2 order-1 md:order-2">
+                        <img
+                            src="/YPV-with-Crystals.jpeg"
+                            alt="YPV-with-Crystals"
+                            className="rounded-lg shadow-lg w-full object-contain max-h-[400px]"
+                        />
+                    </div>
+
+                    {/* Text Section */}
+                    <div className="md:w-1/2 text-center md:text-left space-y-4 order-2 md:order-1 text-lg">
+                        <p className="text-neutral-400">
+                            <b>Pre-requisite :</b> YPV Level 3
+                        </p>
+                        <p className="text-neutral-400">
+                            <b>Duration :</b> 1 day
+                        </p>
+                        <p className="text-neutral-400">
+                            <b>Workshop Features:</b><br />
+                            <li>Healing with Crystals is several times more powerful.</li>
+                            <li>A deeper and faster healing can be accomplished in a short time.</li>
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            {/* Yoga Prana Vidya Level for Psychic Self Protection */}
+            <div className="bg-neutral-900 min-h-screen flex flex-col items-center justify-center space-y-8 px-4 pt-[2rem] pb-[2rem]">
+                <h1 className="text-4xl font-bold text-center">Yoga Prana Vidya Level for Psychic Self Protection</h1>
+                <div className="flex flex-col md:flex-row items-center md:space-x-6 space-y-6 md:space-y-0">
+                    {/* Image Section */}
+                    <div className="md:w-1/2 order-1 md:order-2">
+                        <img
+                            src="/YPV-Level-5.jpeg"
+                            alt="YPV-Level-5"
+                            className="rounded-lg shadow-lg w-full object-contain max-h-[400px]"
+                        />
+                    </div>
+
+                    {/* Text Section */}
+                    <div className="md:w-1/2 text-center md:text-left space-y-4 order-2 md:order-1 text-lg">
+                        <p className="text-neutral-400">
+                            <b>Pre-requisite :</b> YPV Level 3
+                        </p>
+                        <p className="text-neutral-400">
+                            <b>Duration :</b> 1 day
+                        </p>
+                        <p className="text-neutral-400">
+                            <b>Workshop Features:</b><br />
+                            <li>Improve your health, performance and quality of living by learning to protect yourself and your loved ones from detrimental thought forces and emotions of others which may be intentional or unintentional.</li>
+                            <li>Purify home and office from such undesirable contamination occurring continuously.</li>
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            {/* Yoga Prana Vidya Level for Manifestation */}
+            <div className="min-h-screen flex flex-col items-center justify-center space-y-8 px-4 pt-[2rem] pb-[2rem]">
+                <h1 className="text-4xl font-bold text-center">Yoga Prana Vidya Level for Manifestation</h1>
+                <div className="flex flex-col md:flex-row items-center md:space-x-6 space-y-6 md:space-y-0">
+                    {/* Image Section */}
+                    <div className="md:w-1/2 order-1 md:order-2">
+                        <img
+                            src="/YPV-Manifestation.jpeg"
+                            alt="YPV-Manifestation"
+                            className="rounded-lg shadow-lg w-full object-contain max-h-[400px]"
+                        />
+                    </div>
+
+                    {/* Text Section */}
+                    <div className="md:w-1/2 text-center md:text-left space-y-4 order-2 md:order-1 text-lg">
+                        <p className="text-neutral-400">
+                            <b>Pre-requisite :</b> YPV Level 1
+                        </p>
+                        <p className="text-neutral-400">
+                            <b>Duration :</b> 1 day
+                        </p>
+                        <p className="text-neutral-400">
+                            <b>Workshop Features:</b><br />
+                            <li>Learn the techniques to make right and focused targets and manifest them.</li>
+                            <li>Learn how to purge out poverty consciousness and negative thoughts about your own success from your system.</li>
+                            <li>Unlock techniques on how to generate entitlement, how to sustain and maintain it.</li>
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            {/* Yoga Prana Vidya Vaastu */}
+            <div className="bg-neutral-900 min-h-screen flex flex-col items-center justify-center space-y-8 px-4 pt-[2rem] pb-[2rem]">
+                <h1 className="text-4xl font-bold text-center">Yoga Prana Vidya Vaastu</h1>
+                <div className="flex flex-col md:flex-row items-center md:space-x-6 space-y-6 md:space-y-0">
+                    {/* Image Section */}
+                    <div className="md:w-1/2 order-1 md:order-2">
+                        <img
+                            src="/YPV-Vaastu.jpeg"
+                            alt="YPV-Vaastu"
+                            className="rounded-lg shadow-lg w-full object-contain max-h-[400px]"
+                        />
+                    </div>
+
+                    {/* Text Section */}
+                    <div className="md:w-1/2 text-center md:text-left space-y-4 order-2 md:order-1 text-lg">
+                        <p className="text-neutral-400">
+                            <b>Pre-requisite :</b> YPV Level 2
+                        </p>
+                        <p className="text-neutral-400">
+                            <b>Duration :</b> 1 day
+                        </p>
+                        <p className="text-neutral-400">
+                            <b>Workshop Features:</b><br />
+                            <li>Acquire knowledge and techniques for improving home and business prosperity using the science of direction.</li>
+                            <li>Enhance harmony in your environment.</li>
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            {/* Arhat Yoga (Preliminary)*/}
+            <div className="min-h-screen flex flex-col items-center justify-center space-y-8 px-4 pt-[2rem] pb-[2rem]">
+                <h1 className="text-4xl font-bold text-center">Arhat Yoga (Preliminary)</h1>
+                <div className="flex flex-col md:flex-row items-center md:space-x-6 space-y-6 md:space-y-0">
+                    {/* Image Section */}
+                    <div className="md:w-1/2 order-1 md:order-2">
+                        <img
+                            src="/AY.jpeg"
+                            alt="Arhat Yoga"
+                            className="rounded-lg shadow-lg w-full object-contain max-h-[400px]"
+                        />
+                    </div>
+
+                    {/* Text Section */}
+                    <div className="md:w-1/2 text-center md:text-left space-y-4 order-2 md:order-1 text-lg">
+                        <p className="text-neutral-400">
+                            <b>Pre-requisite :</b> YPV Level 3 and AUWA
+                        </p>
+                        <p className="text-neutral-400">
+                            <b>Duration :</b> 2 days
+                        </p>
+                        <p className="text-neutral-400">
+                            <b>Workshop Features:</b><br />
+                            <li>Arhat Yoga is a blend of various yogic systems (Bhakti Yoga, Jnana Yoga, Karma Yoga, Raja Yoga, Dhyana Yoga, Kundalini Yoga, Hatha Yoga). It includes various techniques for purification of mind; character building being given utmost importance. They are special workshops with very powerful yet safe techniques for rapid spiritual progress. These skills are further accompanied by practice of higher and very advanced form of meditations.</li>
+                            <li>Arhat Yoga ( Preliminary) is the first workshop under the Arhat Yoga Program.</li>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
